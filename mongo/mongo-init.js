0af54361bdd4115f59d1bac09d71bc2b8c825671
ddtxra <ddtxra@gmail.com>
2023-06-09 22:41:46 +0200

    adding mongo script

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -130,19 +130,121 @@ function generateRandomPatientValues(patient_id, key, startDate, endDate, count)
     return values;
 }
 
+var edsCounter = 1;
+var patientIdCounter = 1;
+const medicalServices = ["Intensive Care", "Emergency", "Cardiology", "Radiology", "Neurology"];
+
+function generateMedicalServiceAssignments(numPassages) {
+    const assignments = [];
+    const numUnits = Math.min(numPassages, medicalServices.length);
+
+    // Shuffle the medical services array
+    const shuffledServices = medicalServices.slice();
+    for (let i = shuffledServices.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledServices[i], shuffledServices[j]] = [shuffledServices[j], shuffledServices[i]];
+    }
+
+    // Assign medical services to units
+    for (let i = 0; i < numUnits; i++) {
+        const unitMnemo = `UNIT-${String.fromCharCode(65 + i)}`;
+        const medicalService = shuffledServices[i];
+        assignments.push({ unitMnemo, medicalService });
+    }
+
+    // Assign remaining units randomly to medical services
+    for (let i = numUnits; i < numPassages; i++) {
+        const unitMnemo = `UNIT-${String.fromCharCode(65 + i)}`;
+        const medicalService = medicalServices[Math.floor(Math.random() * medicalServices.length)];
+        assignments.push({ unitMnemo, medicalService });
+    }
+
+    return assignments;
+}
+
+function generatePassages(eds, beginDate, endDate, numPassages, assignments) {
+    const passages = [];
+    let currentDate = new Date(beginDate);
+
+    for (let i = 0; i < numPassages; i++) {
+        const passage = {
+            _id: `${eds._id}_passage${i + 1}`,
+            begin_date: new Date(currentDate),
+            end_date: null,
+            care_unit: {
+                mnemo: assignments[i].unitMnemo
+            },
+            sermed: {
+                label: assignments[i].medicalService
+            }
+        };
+
+        // Generate a random duration for the passage in minutes (between 1 and 1440 minutes)
+        const durationInMinutes = Math.floor(Math.random() * 1440) + 1;
+
+        // Update the end date based on the duration
+        const endDateInMillis = currentDate.getTime() + durationInMinutes * 60000;
+        passage.end_date = new Date(endDateInMillis);
+
+        passages.push(passage);
+
+        // Update the current date for the next passage
+        const daysToAdd = Math.floor(Math.random() * 19) + 2;
+        currentDate.setDate(currentDate.getDate() + daysToAdd);
+    }
+
+    // Set the endDate of the last passage to match the endDate of the EDS
+    passages[numPassages - 1].end_date = endDate;
+
+    return passages;
+}
+
+function generateEDS(beginDate, patient_id) {
+    const eds = {
+        _id: "eds_" + edsCounter++,
+        begin_date: beginDate,
+        end_date: null,
+        patient_id: patient_id,
+        type_code: Math.random() < 0.5 ? "HOSP." : "AMBU.",
+        passages: []
+    };
+
+    // Calculate the random number of days to add between 2 and 90 days
+    const daysToAdd = Math.floor(Math.random() * 89) + 2;
+
+    // Create a new Date object based on the beginDate and add the calculated days
+    const endDate = new Date(beginDate);
+    const durationInMinutes = Math.floor(Math.random() * 1440) + 1;
+    endDate.setDate(endDate.getDate() + daysToAdd);
+
+    const numPassages = Math.floor(Math.random() * 5) + 1;
+    const assignments = generateMedicalServiceAssignments(numPassages);
+
+    // Generate passages
+    const passages = generatePassages(eds, beginDate, endDate, numPassages, assignments);
+
+    // Update the endDate of the EDS to match the endDate of the last passage
+    eds.end_date = passages[numPassages - 1].end_date;
+
+    eds.passages = passages;
+
+    return eds;
+}
 
 for (var batchStart = 0; batchStart < totalRecords; batchStart += batchSize) {
     var batchEnd = Math.min(batchStart + batchSize, totalRecords);
 
     var batchInsert = [];
     var values = [];
+    var edss = [];
+
     for (var i = batchStart; i < batchEnd; i++) {
         var randomGender = genders[Math.floor(Math.random() * genders.length)];
         var randomYear = Math.floor(Math.random() * (maxYear - minYear + 1)) + minYear;
         var randomMonth = Math.floor(Math.random() * 12) + 1;
         var randomDay = Math.floor(Math.random() * 30) + 1; // Assuming all months have 30 days for simplicity
 
-        var patient_id = "pat_" + (batchStart + i);
+        var patient_id = "pat_" + patientIdCounter++;
 
         var last_name = generateRandomName("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3, 5);
         var first_name = generateRandomName("abcdefghijklmnopqrstuvwxyz", 4, 8);
@@ -162,12 +264,20 @@ for (var batchStart = 0; batchStart < totalRecords; batchStart += batchSize) {
             sex: randomGender
         });
 
-        values = values.concat(generateRandomPatientValues(patient_id, "pv.ta", new Date("2020-01-01"), new Date("2021-01-01"), 3));
+        var patientValuesGenerated = generateRandomPatientValues(patient_id, "pv.ta", new Date("2020-01-01"), new Date("2021-01-01"), 3);
+        patientValuesGenerated.forEach(function(pv){
+            var eds = generateEDS(pv.datetime, pv.patient_id);
+            edss.push(eds);
+            pv.eds_id = eds._id;
+        })
+
+        values = values.concat(patientValuesGenerated);
 
     }
 
     db.patient_values.insertMany(values);
     db.patients.insertMany(batchInsert);
+    db.eds.insertMany(edss);
 
     print(batchStart + " patients created");
 
